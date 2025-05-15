20c9bdd438d6758c3db20b54d0f5f81ef29a449c
MohammadEmad2003 <105463937+MohammadEmad2003@users.noreply.github.com>
2025-05-15 19:06:47 +0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 // Base URLs for API and WebSocket connections
 export const BASE_URL = "https://daring-anemone-joint.ngrok-free.app";
-export const WS_URL = "wss://daring-anemone-joint.ngrok-free.app ";
+export const WS_URL = "wss://daring-anemone-joint.ngrok-free.app";
 export const ANALYTICS_URL = `${BASE_URL}/model`;
